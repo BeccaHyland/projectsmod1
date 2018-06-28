5a8926af59ad0233946a3306c00dc245d3f826c2
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-06-27 22:05:24 -0600

    corrects and completes Iteration 1

--- credit_check.rb
+++ credit_check.rb
@@ -1,49 +1,51 @@
 # Get the number from the user
 puts "What is your 16-digit card number? Please enter it with no spaces here: > "
+#5541808923795240
 card_number = gets.chomp.to_i
 # make all the characters their own element in an array and reverse the order
-rvrs_card_array = card_number.digits
-
-# select only every other digit starting on the right side
-#p (to check progress here)
-odd_index_num = rvrs_card_array.select.each_with_index { |_, i| i.odd? }
-# trying to choose only every other number? using index?
-#p to check progress
-
-# multiply the selected by 2
-#use a .each with a block and vlock variable - see map ex from lesson
+reverse_card_num = card_number.digits
+#create an empty array for the upcoming doubles (every other doubled) array
 doubles = []
-odd_index_num.each do |number|
-  doubles << number * 2
-end
-#p doubles #to check progress
-# For doubles greater than 9, sum the digits of the products
-revised_doubles = []
-doubles.each do |number|
-  if number > 9
-    number = number.to_s
-    first_dig = "to be replaced"
-    first_dig = number[0]
-    first_dig = first_dig.to_i
-    second_dig = "replace me too"
-    second_dig = number[1]
-    second_dig = second_dig.to_i
-    # Maybe the below line is the problem.
-    number = first_dig + second_dig
+# double only every other digit starting on the right side
+#use a .each with a block and vlock variable - see map ex from lesson
+reverse_card_num.each.with_index do |digit, index|
+  if index.odd?
+    doubles << digit * 2
   else
-
+    doubles << digit
   end
 end
-  revised_doubles << doubles
-  # HMM...the below check did not wor
-  # HMM...the output was 8 instances of the unchanged array named DOUBLES
-  p revised_doubles
-# Add all the revised doubled numbers together
-# rev_doub_sum = revised_doubles.sum
+#create new array to store version with double-digit numbers changed so the are instead the
+corrected_doubles = []
+doubles.each.with_index do |digit, index|
+  if digit > 9
+    digit = digit.to_s.chars
+    first_char = 0
+    first_char = digit[0]
+    second_char = 0
+    second_char = digit[1]
+    digit = first_char.to_i + second_char.to_i
+    corrected_doubles << digit
+  else
+    corrected_doubles << digit
+  end
+end
+# started other way but it will not work
+#     digit = digit.to_s
+#     digit = digit[0]
+#     corrected_doubles << digit[0] + digit[1]
 
-# get the sum of the even index numbers
-# add both those sums together
-# Divide the sum by 10
-# / 10
-# if the result is zero, print "The number is valid!"
-# if it is not zero, print "The number is invalid."
+#p corrected_doubles -- GOOD ARRAY!!! :)
+sum = corrected_doubles.sum
+#p (result: sum = 70)
+
+# # Divide the sum by 10
+
+if sum % 10 == 0
+  puts "The number is valid!"
+else
+  puts "The number is invalid."
+end
+
+# # if the result is zero, print "The number is valid!"
+# # if it is not zero, print "The number is invalid."
